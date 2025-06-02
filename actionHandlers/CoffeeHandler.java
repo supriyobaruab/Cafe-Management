eaf5f46151c71415c793bf36ece7634140434332
supriyob <supriyob@gmail.com>
2025-06-02 13:09:58 +0600

    added the logic for americano button

--- actionHandlers/CoffeeHandler.java
+++ actionHandlers/CoffeeHandler.java
@@ -1,33 +1,54 @@
 package actionHandlers;
+
 import java.awt.event.ActionEvent;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 
 public class CoffeeHandler {
-    private JRadioButton americano,espresso,latte,cappuccino;
-    private int aq,eq,lq,cq;
+    private JRadioButton americano, espresso, latte, cappuccino;
+    private int aq, eq, lq, cq;
 
-    public CoffeeHandler(JRadioButton americano,JRadioButton espresso,JRadioButton latte,JRadioButton cappuccino){
+    public CoffeeHandler(JRadioButton americano, JRadioButton espresso, JRadioButton latte, JRadioButton cappuccino) {
         this.americano = americano;
         this.espresso = espresso;
         this.latte = latte;
         this.cappuccino = cappuccino;
-        aq=0;
-        eq=0;
-        lq=0;
-        cq=0;
-        }
-        public void handle(ActionEvent ae){
-            if(americano.isSelected()){
-                String asq = JOptionPane.showInputDialog("Enter the quantity");
-                System.out.print(asq);
-                if(asq == null || asq == ""){
-                    JOptionPane.showMessageDialog(null,"Enter a valid number");
-                    asq = JOptionPane.showInputDialog("Enter the quantity");
+        aq = 0;
+        eq = 0;
+        lq = 0;
+        cq = 0;
+    }
+
+    public void handle(ActionEvent ae) {
+        String aqs;
+        boolean valid = false;
+        if (americano.isSelected()) {
+            while (!valid) {
+                aqs = JOptionPane.showInputDialog("Enter the quantity");
+
+                if (aqs == null) {
+                    americano.setSelected(false);
+                    break;
                 }
-                aq = Integer.parseInt(asq);
-                System.out.println(asq);
+
+                if (aqs.trim().equals("")) {
+                    JOptionPane.showMessageDialog(null, "Enter a valid number");
+                    continue;
+                }
+
+                try {
+                    int quantity = Integer.parseInt(aqs.trim());
+                    if (quantity > 0) {
+                        valid = true;
+                        aq = quantity;
+                        System.out.println("Quantity entered: " + quantity);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Please enter a positive number");
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Please enter a numeric value");
+                }
             }
         }
-    
+    }
 }
